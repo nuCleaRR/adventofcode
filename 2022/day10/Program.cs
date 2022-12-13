8c38dc075f0d1ff8e567368a4094dab5b5167385
Alexey Novitskyi <iamalexey@gmail.com>
2022-12-13 01:07:47 +0100

    Day10 solved tasks

---
+++ 2022/day10/Program.cs
@@ -0,0 +1,65 @@
+﻿using Day10;
+using Microsoft.Win32;
+using System.Text.RegularExpressions;
+
+var inputFile = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "input.txt"));
+var input = inputFile.Split(Environment.NewLine).ToList();
+
+var task1Result = 0;
+var totalRegister = 1;
+var tickCount = 1;
+
+for (int i = 0; i < input.Count; i++)
+{
+    var command = input[i];
+
+    if (command == "noop")
+    {
+        Tick();
+    }
+    else
+    {
+        Tick();
+        Tick();
+
+        var register = int.Parse(Regex.Match(command, @"addx (-?\d+)").Groups[1].ValueSpan);
+        totalRegister += register;
+    }
+}
+
+void Tick()
+{
+    const int startValue = 20;
+    const int delta = 40;
+
+    if (((tickCount - startValue) % delta) == 0)
+    {
+        task1Result += totalRegister * (tickCount);
+    }
+
+    Draw();
+
+    tickCount++;
+}
+
+void Draw()
+{
+    var sprite = (tickCount - 1) % 40;
+
+    if (sprite == 0)
+    {
+        Console.WriteLine();
+    }
+
+    if (sprite == totalRegister || sprite == totalRegister - 1 || sprite == totalRegister + 1)
+    {
+        Console.Write("#");
+    }
+    else
+    {
+        Console.Write(".");
+    }
+}
+
+Console.WriteLine($"Task 1 result {task1Result}"); //15120
+Console.WriteLine($"Task 1 result RKPJBPLA"); //RKPJBPLA
